0e71b58e83ee1958909563b0cc7997e55a1baa6a
leonardo pantano <leonardo.pantano@saveincloud.com>
2026-08-31 15:05:07 -0300

    add

--- public/api.php
+++ public/api.php
@@ -170,13 +170,6 @@ $rawInput =
 
 $data = [];
 
-if ($_GET['action'] ?? '' === 'debug') {
-    responder([
-        'raw_input' => file_get_contents('php://input'),
-        'content_type' => $_SERVER['CONTENT_TYPE'] ?? '',
-        'method' => $_SERVER['REQUEST_METHOD'] ?? '',
-    ]);
-}
 if (
     $rawInput !== false &&
     trim($rawInput) !== ''
